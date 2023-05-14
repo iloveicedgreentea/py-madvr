847f646ae8522783b27c21872328cea5bc29197b
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2023-05-14 14:25:03 -0400

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_madvr",
-    version="1.3.17",
+    version="1.3.18",
     author="iloveicedgreentea",
     description="A package to control MadVR Envy over IP",
     long_description=long_description,
