aa6b461fa375bb45c7bd8deed7f369b254ec914a
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-06-29 12:41:08 -0400

    add error class

--- madvr/errors.py
+++ madvr/errors.py
@@ -1,5 +1,7 @@
 """Errors for madvr"""
 
+from __future__ import annotations
+
 
 class AckError(Exception):
     """An error when ACK is not correct"""
@@ -11,3 +13,7 @@ class RetryExceededError(Exception):
 
 class HeartBeatError(Exception):
     """An error has occured with heartbeats"""
+
+
+class CannotConnect(Exception):
+    """Error to indicate we cannot connect."""
